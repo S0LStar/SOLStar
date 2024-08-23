5d25ebdf606a371be9a9005720584dec07e06e41
player397 <tmdxkr5@hanmail.net>
2024-08-23 23:00:52 +0900

    chore: FE init

--- solstar/dev-dist/sw.js
+++ solstar/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0o3e2hefgr"
+    "revision": "0.ic3n1r1o5uo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
