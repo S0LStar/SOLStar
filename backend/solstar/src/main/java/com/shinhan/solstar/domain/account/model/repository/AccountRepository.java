9532a192955b9447a827b0aff1b55fe7c9ee7b2b
kimseonha <ksh219805@gmail.com>
2024-08-28 00:49:56 +0900

    Fix: 기존 폴더 삭제

--- backend/solstar/src/main/java/com/shinhan/solstar/domain/account/model/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shinhan.solstar.domain.account.model.repository;
-
-import com.shinhan.solstar.domain.account.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
