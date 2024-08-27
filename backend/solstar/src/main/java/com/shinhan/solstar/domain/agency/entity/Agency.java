c4b7d7209f2c81e5e7357e16955214795cae1bf0
player397 <107818612+player397@users.noreply.github.com>
2024-08-27 15:43:11 +0900

    Revert "Revert "develop으로 develop-be 머지""

--- backend/solstar/src/main/java/com/shinhan/solstar/domain/agency/entity/Agency.java
+++ backend/solstar/src/main/java/com/shinhan/solstar/domain/agency/entity/Agency.java
@@ -5,13 +5,19 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Agency {
+public class Agency implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,7 +31,7 @@ public class Agency {
     private String email;
 
     @Column(name = "password", nullable = false, length = 255)
-    private String passowrd;
+    private String password;
 
     @Column(name = "phone", nullable = false, length = 45)
     private String phone;
@@ -36,4 +42,42 @@ public class Agency {
     @Column(name = "is_delete", nullable = false)
     private boolean isDelete;
 
+    public void updateRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("AGENCY"));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
