e9fcc5fe2e6da5429278d6dd1ff01161f4abb2f1
이지혜 <hyeee19@naver.com>
2024-08-28 01:12:43 +0900

    Desing: 선호 아티스트 펀딩 전체보기 페이지 css

--- frontend/solstar/src/components/funding/common/FundingCard.jsx
+++ frontend/solstar/src/components/funding/common/FundingCard.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import temp from '../../../assets/character/Sol.png'; // 임시 이미지 경로
 import './FundingCard.css';
 import { useEffect } from 'react';
+import Certification from '../../../assets/common/Certification.png';
 
 function FundingCard({ funding }) {
   useEffect(() => {
@@ -10,15 +11,18 @@ function FundingCard({ funding }) {
   });
 
   return (
-    <div className="search-result-funding-item">
-      <img src={temp} alt="" className="search-result-funding-image" />
-      <div className="search-result-funding-info">
-        <span className="search-result-funding-artist">
-          {funding.artistName}
-        </span>
-        <h3 className="search-result-funding-title">{funding.title}</h3>
+    <div className="funding-card-item">
+      <div className="funding-card-image-container">
+        <img src={temp} alt="img" className="funding-card-image" />
+        {funding.type === 'VERIFIED' && (
+          <img src={Certification} className="certification-mark"></img>
+        )}
+      </div>
+      <div className="funding-card-info">
+        <span className="funding-card-artist">{funding.artistName}</span>
+        <h3 className="funding-card-title">{funding.title}</h3>
         <div
-          className={`search-result-funding-details 
+          className={`funding-card-details 
             ${
               funding.status === 'PROCESSING'
                 ? 'progress'
@@ -28,24 +32,24 @@ function FundingCard({ funding }) {
             }`}
           style={{ '--achievement-percentage': funding.successRate }}
         >
-          <div className="search-result-funding-detail">
-            <span className="search-result-funding-progress">
+          <div className="funding-card-detail">
+            <span className="funding-card-progress">
               {funding.successRate}%
             </span>
-            <span className="search-result-funding-amount">
+            <span className="funding-card-amount">
               {funding.totalAmount.toLocaleString()}원
             </span>
             {funding.remainDays ? (
-              <span className="search-result-funding-days-left">
+              <span className="funding-card-days-left">
                 {funding.remainDays}일 남음
               </span>
             ) : funding.status === 'SUCCESS' ? (
-              <span className="search-result-funding-days-left">펀딩 성공</span>
+              <span className="funding-card-days-left">펀딩 성공</span>
             ) : (
-              <span className="search-result-funding-days-left">펀딩 무산</span>
+              <span className="funding-card-days-left">펀딩 무산</span>
             )}
           </div>
-          <div className="popular-funding-achievement-bar"></div>
+          <div className="funding-card-achievement-bar"></div>
         </div>
       </div>
     </div>
--- frontend/solstar/src/components/main/myartist/MyArtistFundingContainer.jsx
+++ frontend/solstar/src/components/main/myartist/MyArtistFundingContainer.jsx
@@ -1,5 +1,67 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import BackButton from '../../common/BackButton';
+import FundingCard from '../../funding/common/FundingCard';
+import './MyArtistFundingContainer.css';
+
 function MyArtistFundingContainer() {
-  return <div>나의 선호 아티스트 펀딩 전체</div>;
+  const navigate = useNavigate();
+  const [fundings, setFundings] = useState([]);
+
+  useEffect(() => {
+    // TODO : 나의 선호 아티스트 펀딩 리스트 api 연결
+    // 선호 아티스트 펀딩 리스트 Data
+    const fetchFunding = [
+      {
+        fundingId: 1,
+        type: 'VERIFIED',
+        artistName: '뉴진스',
+        title: '뉴진스 데뷔 2주년 기념🎉 2호선을 뉴진스로 물들여요!',
+        fundingImage: 'image',
+        successRate: 372,
+        totalAmount: 18600000,
+        status: 'PROCESSING',
+        remainDays: 22,
+      },
+      {
+        fundingId: 2,
+        type: 'COMMON',
+        artistName: '민지 (NewJeans)',
+        title:
+          '뉴진스 민지의 이름으로 따뜻한 마음을 전해요 💙 펀딩이 함께하는 사랑의 기부',
+        fundingImage: '../../../assets/character/Sol.png',
+        successRate: 160,
+        totalAmount: 1600000,
+        status: 'SUCCESS',
+        remainDays: null,
+      },
+    ];
+
+    setFundings(fetchFunding);
+  }, []);
+
+  return (
+    <div className="my-artist-funding-container">
+      <header className="my-artist-funding-header">
+        <BackButton />
+        <div className="my-artist-funding-header-description">
+          나의 선호 아티스트 펀딩
+        </div>
+      </header>
+      <div className="my-artist-funding-list">
+        {fundings.map((funding, index) => (
+          <FundingCard
+            key={funding.fundingId}
+            index={index}
+            funding={funding}
+            onClick={() => {
+              navigate(`/funding/${funding.fundingId}`);
+            }}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default MyArtistFundingContainer;
