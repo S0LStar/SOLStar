155e279c417df245bf30d6bb4411e86987896f61
kimseonha <ksh219805@gmail.com>
2024-08-31 02:19:11 +0900

    Feat:중복검사추가

---
+++ backend/src/main/java/com/common/solstar/domain/auth/dto/request/EmailCheckDuplicateRequest.java
@@ -0,0 +1,16 @@
+package com.common.solstar.domain.auth.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailCheckDuplicateRequest {
+
+    private String email;
+
+}
